6c3822473ec67043e0136d8450ddf101089aad74
Shubham Kumar <sk8340247116@gmail.com>
2026-02-14 10:58:53 +0530

    feat: connect frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://secure-auth-backend-fx2c.onrender.com/api",
 });
 
 // 🔥 auto attach token
